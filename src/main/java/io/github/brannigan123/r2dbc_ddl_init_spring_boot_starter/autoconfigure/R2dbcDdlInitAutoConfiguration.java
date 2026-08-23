8e7e46a2e709d06cb2ca8c5830d9b6b3b2662986
Brannigan Sakwah <brannigansakwah@gmail.com>
2026-08-23 11:26:58 +0300

    fix downstream discovery of CommonR2dbcRepositoryImpl

--- src/main/java/io/github/brannigan123/r2dbc_ddl_init_spring_boot_starter/autoconfigure/R2dbcDdlInitAutoConfiguration.java
+++ src/main/java/io/github/brannigan123/r2dbc_ddl_init_spring_boot_starter/autoconfigure/R2dbcDdlInitAutoConfiguration.java
@@ -3,6 +3,8 @@ package io.github.brannigan123.r2dbc_ddl_init_spring_boot_starter.autoconfigure;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -11,6 +13,7 @@ import org.springframework.data.r2dbc.convert.R2dbcCustomConversions;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.r2dbc.dialect.PostgresDialect;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.data.r2dbc.repository.support.R2dbcRepositoryFactoryBean;
 import org.springframework.data.relational.core.mapping.RelationalMappingContext;
 
 import io.github.brannigan123.r2dbc_ddl_init_spring_boot_starter.config.R2dbcSchemaInitializer;
@@ -38,6 +41,18 @@ public class R2dbcDdlInitAutoConfiguration {
         converters.add(new GenericEnumToStringConverter());
         converters.add(new GenericStringToEnumConverterFactory());
         return R2dbcCustomConversions.of(PostgresDialect.INSTANCE, converters);
+    }
+
+    @Bean
+    public static BeanFactoryPostProcessor r2dbcRepositoryBaseClassPostProcessor() {
+        return beanFactory -> {
+            for (String beanName : beanFactory.getBeanDefinitionNames()) {
+                BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanName);
+                if (R2dbcRepositoryFactoryBean.class.getName().equals(beanDefinition.getBeanClassName())) {
+                    beanDefinition.getPropertyValues().add("repositoryBaseClass", CommonR2dbcRepositoryImpl.class);
+                }
+            }
+        };
     }
 
     @Bean
